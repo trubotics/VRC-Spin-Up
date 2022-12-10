daba08068f9e55b0dc8e880ea6a60df0f6c78f0d
Lock-The-Door <Lock-The-Door@outlook.com>
2022-12-10 14:22:09 -0500

    Initial Commit Includes manual, untested, mechnum wheel, arcade driving controls!!!

---
+++ src/main.cpp
@@ -0,0 +1,124 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       Trubotics                                                  */
+/*    Created:      2022-12-09, 10:36:28 a.m.                                    */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+#include "vex.h"
+
+using namespace vex;
+
+// A global instance of competition
+competition Competition;
+
+// define your global instances of motors and other devices here
+
+// controller
+controller primaryController = controller(primary);
+
+// drive train
+motor frontLeft = motor(PORT1, ratio18_1, false);
+motor backLeft = motor(PORT2, ratio18_1, false);
+motor frontRight = motor(PORT3, ratio18_1, true);
+motor backRight = motor(PORT4, ratio18_1, true);
+
+/* Global Functions */
+
+// mech drive
+void drive(int forward, int right, int turn)
+{
+  frontRight.spin(vex::forward, forward - right + turn, vex::percent);
+  frontLeft.spin(vex::forward, forward + right - turn, vex::percent);
+  backRight.spin(vex::forward, forward + right + turn, vex::percent);
+  backLeft.spin(vex::forward, forward - right - turn, vex::percent);
+}
+
+/*---------------------------------------------------------------------------*/
+/*                          Pre-Autonomous Functions                         */
+/*                                                                           */
+/*  You may want to perform some actions before the competition starts.      */
+/*  Do them in the following function.  You must return from this function   */
+/*  or the autonomous and usercontrol tasks will not be started.  This       */
+/*  function is only called once after the V5 has been powered on and        */
+/*  not every time that the robot is disabled.                               */
+/*---------------------------------------------------------------------------*/
+
+void pre_auton(void)
+{
+
+  // All activities that occur before the competition starts
+  // Example: clearing encoders, setting servo positions, ...
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              Autonomous Task                              */
+/*                                                                           */
+/*  This task is used to control your robot during the autonomous phase of   */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+
+void autonomous(void)
+{
+  // ..........................................................................
+  // Insert autonomous user code here.
+  // ..........................................................................
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              User Control Task                            */
+/*                                                                           */
+/*  This task is used to control your robot during the user control phase of */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+
+void usercontrol(void)
+{
+  // User control code here, inside the loop
+  while (1)
+  {
+    // This is the main execution loop for the user control program.
+    // Each time through the loop your program should update motor + servo
+    // values based on feedback from the joysticks.
+
+    // ........................................................................
+    // Insert user code here. This is where you use the joystick values to
+    // update your motors, etc.
+    // ........................................................................
+
+    // arcade drive
+    drive(primaryController.Axis3.position(),
+          primaryController.Axis4.position(),
+          primaryController.Axis1.position());
+
+    wait(20, msec); // Sleep the task for a short amount of time to
+                    // prevent wasted resources.
+  }
+}
+
+//
+// Main will set up the competition functions and callbacks.
+//
+int main()
+{
+  // Set up callbacks for autonomous and driver control periods.
+  Competition.autonomous(autonomous);
+  Competition.drivercontrol(usercontrol);
+
+  // Run the pre-autonomous function.
+  pre_auton();
+
+  // Prevent main from exiting with an infinite loop.
+  while (true)
+  {
+    wait(100, msec);
+  }
+}
